30cec39ffdb56d5fb291b452226d80d563d83ef1
averyocean65 <averytoad65@gmail.com>
2026-01-14 23:19:55 +0100

    final fixes before publish

--- src/org/averyocean65/swp/Program.java
+++ src/org/averyocean65/swp/Program.java
@@ -4,7 +4,7 @@ import org.averyocean65.swp.windows.EditorWindow;
 
 public final class Program {
     public static void main(String[] args) {
-        EditorWindow window = new EditorWindow("Test", 800, 600);
+        EditorWindow window = new EditorWindow("SWP", 800, 600);
         window.setShowing(true);
     }
 }
